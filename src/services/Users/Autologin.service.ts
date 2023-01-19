4d61e5df017bb4437dc9eab5b104c8d2dce6b453
Alex Conder <alex.v.conder@gmail.com>
2023-01-19 13:35:23 -0300

    feat: autologin service

---
+++ src/services/Users/Autologin.service.ts
@@ -0,0 +1,21 @@
+import { ObjectId } from "mongodb";
+import Users from "../../models/Users";
+
+export class UserAutoLogin{
+    async execute(body: any){
+        const { _id } = body;
+        const objectUserId = new ObjectId(_id);
+
+        const user = await Users.findOne({ _id: objectUserId});
+
+        if(!user){
+            throw new Error("O usuário vínculado a está token não existe.");
+        }
+
+        return {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        }
+    }
+}
