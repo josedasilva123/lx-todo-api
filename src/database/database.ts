808e955d183855ecaa4d7295f146c91850522144
Alex Conder <alex.v.conder@gmail.com>
2023-01-19 14:29:20 -0300

    fix: adding strict query config (mongoose 7 rule)

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 export async function connect() {
    try {
       console.log("Conectando...");
+      mongoose.set('strictQuery', true);
       await mongoose.connect(process.env.DATABASE_URL as string);
+      
       console.log("Conexão realizada com sucesso!");
    } catch (error) {
       console.error("Não foi possível conectar!");
