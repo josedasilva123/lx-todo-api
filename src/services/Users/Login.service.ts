062051b986c737311b9e507dba190902a18d329a
Alex Conder <alex.v.conder@gmail.com>
2023-01-19 13:35:56 -0300

    feat: login service

---
+++ src/services/Users/Login.service.ts
@@ -0,0 +1,36 @@
+import Users from "../../models/Users";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export class UserLogin {
+    async execute(body: any){
+        const { email, password } = body;
+
+        const user = await Users.findOne({ email: email });
+
+        if(!user) {
+            throw new Error("O usuário fornecido não está cadastrado.");
+        }
+
+        if(!bcrypt.compareSync(password, user.password)) {
+            throw new Error("Desculpe, o e-mail e senha fornecidos não correspondem.");
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            process.env.JWT_SECRETKEY as string,
+            { expiresIn: "12h"}
+        );
+
+        return {
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+            token: token,
+        }
+    }
+}
