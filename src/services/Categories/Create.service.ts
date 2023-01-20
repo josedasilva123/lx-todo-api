a6addef1898f717d8882e9463b28c6d27f6e0e3b
Alex Conder <alex.v.conder@gmail.com>
2023-01-20 14:16:23 -0300

    identation and text fix

--- src/services/Categories/Create.service.ts
+++ src/services/Categories/Create.service.ts
@@ -1,24 +1,24 @@
 import Categories from "../../models/Categories";
 import { iCategoryCreateBody } from "../../routes/Categories/@types";
 
-export class CategoryCreate{
-    async execute(body: iCategoryCreateBody){
-        const { _id, label, slug } = body;
+export class CategoryCreate {
+   async execute(body: iCategoryCreateBody) {
+      const { _id, label, slug } = body;
 
-        const category = {
-            userId: _id,
-            label,
-            slug
-        }
+      const category = {
+         userId: _id,
+         label,
+         slug,
+      };
 
-        const existingCategory = await Categories.findOne({ slug: slug });
+      const existingCategory = await Categories.findOne({ slug: slug });
 
-        if(existingCategory){
-            throw new Error("Já existe uma categoria cadastrada com este respectivo slug.");
-        }
+      if (existingCategory) {
+         throw new Error("Já existe uma categoria cadastrada com este respectivo slug.");
+      }
 
-        const newCategory = await Categories.create(category);
+      const newCategory = await Categories.create(category);
 
-        return { category: newCategory, message: 'Categoria criada com sucesso!'}
-    }
+      return { category: newCategory, message: "Categoria cadastrada com sucesso!" };
+   }
 }
